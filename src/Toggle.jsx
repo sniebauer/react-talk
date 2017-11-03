a479a43f12dccc05d1c71c2d2a9f10c3886ba141
Narmin <narminshahin@gmail.com>
2017-11-03 15:25:55 -0400

    videoThumbnail and Status in seperate

---
+++ src/Toggle.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Status extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      watched: false
+    }
+    this.onCheckboxClick = this.onCheckboxClick.bind(this);
+  }
+
+  onCheckboxClick () {
+    this.setState({watched: !this.state.watched})
+  }
+
+  render(){
+    console.log('VID ID:: ', this.props.id)
+    return (
+      <div>
+        <button onClick={this.onCheckboxClick}>
+          { this.state.watched ? "Watched" : "Haven't Watched" }
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Status;
---
+++ src/VideoThumbnail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const VideoThumbnail = (props) => {
+  return <div id={props.id} className="thumbnail">
+    <a href={props.url} target="_blank">
+      <img src={props.pic} />
+      <h3 style={ {color:"#00adef"} } className="title">{props.name}</h3>
+    </a>
+  </div>
+}
+
+export default VideoThumbnail;
