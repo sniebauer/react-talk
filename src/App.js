eb13a34729a22b99f583214229a5ff5d1b3564cc
Hannah Nordgren <hey@hereshannahs.info>
2017-11-03 16:57:50 -0400

    [FEATURE] implemented styles on propsDemo branch

--- src/App.js
+++ src/App.js
@@ -19,36 +19,52 @@ const Title = (props) => (
 class App extends Component {
   render() {
     return (
-    <div>
-      <h1>React Talk NYU</h1>
-        <VideoThumbnail url={data.staffPicks[0].link}
-                        pic={data.staffPicks[0].thumbnail}/>
-        <Title name="Working With Jigsaw"/>
+      <div>
+        <h1 className="container-title">NYU WinC Tech Talk</h1>
+        <div className="card-container">
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[0].link}
+                            pic={data.staffPicks[0].thumbnail}/>
+            <Title name="Working With Jigsaw"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[1].link}
-                        pic={data.staffPicks[1].thumbnail}/>
-        <Title name="High Tide"/>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[1].link}
+                            pic={data.staffPicks[1].thumbnail}/>
+            <Title name="High Tide"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[2].link}
-                        pic={data.staffPicks[2].thumbnail}/>
-        <Title name="RICHARD TWICE"/>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[2].link}
+                            pic={data.staffPicks[2].thumbnail}/>
+            <Title name="RICHARD TWICE"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[3].link}
-                        pic={data.staffPicks[3].thumbnail}/>
-        <Title name="FATA MORGANA"/>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[3].link}
+                            pic={data.staffPicks[3].thumbnail}/>
+            <Title name="FATA MORGANA"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[4].link}
-                        pic={data.staffPicks[4].thumbnail}/>
-        <Title name="Every Nite Is Emo Nite"/>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[4].link}
+                            pic={data.staffPicks[4].thumbnail}/>
+            <Title name="Every Nite Is Emo Nite"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[5].link}
-                        pic={data.staffPicks[5].thumbnail}/>
-        <Title name="Dahlia (2017)"/>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[5].link}
+                            pic={data.staffPicks[5].thumbnail}/>
+            <Title name="Dahlia (2017)"/>
+          </div>
 
-        <VideoThumbnail url={data.staffPicks[6].link}
-                        pic={data.staffPicks[6].thumbnail}/>
-        <Title name="In a Heartbeat - Animated Short Film"/>        
-    </div>
+          <div className="card">
+            <VideoThumbnail url={data.staffPicks[6].link}
+                            pic={data.staffPicks[6].thumbnail}/>
+            <Title name="In a Heartbeat - Animated Short Film"/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
